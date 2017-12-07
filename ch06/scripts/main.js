49cc0ca477e62341e27230b4526115a8e466b700
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-06 22:18:38 -0300

    Working with Query Selector

---
+++ ch06/scripts/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by barcvilla on 29/11/2017.
+ */
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
